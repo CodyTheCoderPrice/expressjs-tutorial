9b9e145446f7782002048a182c3461b04605fd7f
Cody Price <CodyWilliamPrice@outlook.com>
2024-05-07 11:45:22 -0400

    Used post, put, patch, and delete

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,8 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
@@ -26,16 +28,57 @@ app.get('/api/users', (req, res) => {
 	return res.send(mockUsers);
 });
 
+app.post('/api/users', (req, res) => {
+	const { body } = req;
+	const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body };
+	mockUsers.push(newUser);
+	return res.status(201).send(newUser);
+});
+
 app.get('/api/users/:id', (req, res) => {
 	const parsedId = parseInt(req.params.id);
 	if (isNaN(parsedId)) {
 		return res.status(400).send({ msg: 'Bad Request. Invalid ID.' });
 	}
-
 	const findUser = mockUsers.find((user) => user.id === parsedId);
 	if (!findUser) return res.sendStatus(404);
-
 	return res.send(findUser);
+});
+
+app.put('/api/users/:id', (req, res) => {
+	const {
+		body,
+		params: { id },
+	} = req;
+	const parsedId = parseInt(id);
+	if (isNaN(parsedId)) return res.sendStatus(400);
+	const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+	if (findUserIndex === -1) return res.sendStatus(404);
+	mockUsers[findUserIndex] = { id: parsedId, ...body };
+	return res.sendStatus(200);
+});
+
+app.patch('/api/users/:id', (req, res) => {
+	const {
+		body,
+		params: { id },
+	} = req;
+	const parsedId = parseInt(id);
+	if (isNaN(parsedId)) return res.sendStatus(400);
+	const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+	if (findUserIndex === -1) return res.sendStatus(404);
+	mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
+	return res.sendStatus(200);
+});
+
+app.delete('/api/users/:id', (req, res) => {
+	const { id } = req.params;
+	const parsedId = parseInt(id);
+	if (isNaN(parsedId)) return res.sendStatus(400);
+	const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+	if (findUserIndex === -1) return res.sendStatus(404);
+	mockUsers.splice(findUserIndex, 1);
+	return res.sendStatus(200);
 });
 
 app.get('/api/products', (req, res) => {
